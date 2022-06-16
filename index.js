fe42cc5900afca00537ddfd0b90bad872c099474
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-16 05:12:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals;
+const [bessie, , dolly, babe, little] = farmAnimals;
+const [blackAndWhite, , black, pink, ] = farmAnimals
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg, ] = colors;
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+    muppetName: 'Miss Piggy',
+    color: 'pink',
+    song: 'Never Before, Never Again',
+    job: 'Cast member of The Muppet Show',
+    partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
+    nestedName: 'Kermit',
+    nestedColor: 'green',
+    album: {
+        theMuppetMovie: {
+            song1: 'Rainbow Connection',
+            song2: 'Moving Right Along',
+            song3: 'Never Before, Never Again',
+            song4: 'I Hope That Something Better Comes Along',
+        },
     },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+    nestedJob: 'Host of The Muppet Show',
+    nestedPartner: 'Miss Piggy'
 };
-
-// Strings
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+const { nestedJob, nestedPartner } = nestedMuppet; // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
